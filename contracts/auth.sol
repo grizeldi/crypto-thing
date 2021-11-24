594673bcff1cb6074e3b109e319600038f06f057
grizeldi <grizeldi@gmail.com>
2021-11-24 19:37:52 +0100

    Initial authentication contract

---
+++ contracts/auth.sol
@@ -0,0 +1,49 @@
+//SPDX-License-Identifier: UNLICENCED
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract AuthContract {
+    using Chainlink for Chainlink.Request;
+    
+    // API authentication stuff
+    address private owner;
+    string private authToken = "";
+    
+    // Oracle stuff
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+    
+    constructor() {
+        owner = msg.sender;
+        
+        setPublicChainlinkToken();
+        oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
+        jobId = "d5270d1c311941d0b08bead21fea7747";
+        fee = 0.1 * 10 ** 18;
+    }
+    
+    function requestData(int dataId) public{
+        address userAddress = msg.sender;
+        
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        //This will be done over https in an actual deployment
+        request.add("get", "http://ourapi.herokuapp.com/?apiKey=" + authToken);
+        request.add("path", "data");
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+    
+    function fulfill(bytes32 _requestId, string _data) public recordChainlinkFulfillment(_requestId){
+        //TODO callback the original caller
+    }
+    
+    function setAuthToken(string memory _authToken) public onlyOwner{
+        authToken = _authToken;
+    }
+    
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+}

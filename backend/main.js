01a0bab7212b0af51319be9e84448b90468856e2
grizeldi <grizeldi@gmail.com>
2021-12-27 18:12:05 +0100

    Added web3 dependency

--- backend/main.js
+++ backend/main.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.static("frontend"));
+// Frontend dependencies that are installed via npm
 app.use(express.static("node_modules/jquery/dist"));
+app.use(express.static("node_modules/web3/dist"));
 
 console.log("Listening on port: " + PORT);
 app.listen(PORT);

5f62b514a2a47004c1b90b5108185ef61a679288
grizeldi <grizeldi@gmail.com>
2021-12-30 14:52:23 +0100

    Added hardcoded stuff so frontend dev can progress

--- contracts/available_ids.sol
+++ contracts/available_ids.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 contract DataIDOracle {
     address private owner;
-    mapping(address => int[]) private cache;
+    mapping(address => string[]) private cache;
 
     event OnDataRequest(address indexed _fromUser);
     event DataAvailable(address indexed _forUser);
@@ -16,13 +16,21 @@ contract DataIDOracle {
     * Called to request the available IDs to the sender.
     */
     function request() public {
-        emit OnDataRequest(msg.sender);
+        //emit OnDataRequest(msg.sender);
+
+        // The following lines are for debugging the frontend, delete when the js part of the oracle works.
+        // Data IDs will need to be changed depending on what's in your database.
+        string[] memory hardcodedIDs = new string[](1);
+        hardcodedIDs[0] = "61cda92ad0730018758b4283";
+        //fulfill(msg.sender, hardcodedIDs); //This won't work because ownerOnly
+        cache[msg.sender] = hardcodedIDs;
+        emit DataAvailable(msg.sender);
     }
 
     /**
     * Called by the js part of the oracle once the data is obtained
     */
-    function fulfill(address forUser, int[] calldata availableIDs) onlyOwner public {
+    function fulfill(address forUser, string[] memory availableIDs) onlyOwner public {
         cache[forUser] = availableIDs;
         emit DataAvailable(forUser);
     }
@@ -30,8 +38,8 @@ contract DataIDOracle {
     /**
     * Called by the requester once the data is available.
     */
-    function readData() public view returns(int[] memory){
-        return cache[msg.sender];
+    function readData() public view returns(string[] memory output){
+        output = cache[msg.sender];
     }
 
     modifier onlyOwner(){
